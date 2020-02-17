ec4835cf89e3fed89ccc3865d71277428f1cc3e5
S-Spencer <45274560+S-Spencer@users.noreply.github.com>
2020-02-17 22:29:28 +0000

    Update day1.py
    Updated to correctly answer part 2.
    Changes made to labelling of "FT" so as to have two distinct values for storing the answers to each part of the puzzle.
    Changes made to spacing in print lines for clearer reading.

--- day1.py
+++ day1.py
@@ -8,7 +8,7 @@ for line in raw:
 raw.close()
 
 """
-Next step calculate the Fuel Total required as the puzzle answer for part one 'FT'
+Next step calculate the Fuel Total required as the puzzle answer for part one 'FT1'
 To calculate fuel for each mass in masses:
 Divide mass by 3
 Take int of the result
@@ -16,9 +16,27 @@ Subtract 2
 Add result to FT
 """
 
-FT = 0
+FT1 = 0
 for mass in masses:
     fuel = int(mass/3)
-    FT += (fuel - 2)
+    FT1 += (fuel - 2)
 
-print("The answer to part 1 is" + str(FT))
+print("The answer to part 1 is " + str(FT1))
+
+"""
+Now the Fuelt Total 'FT2' must take into account the addition mass created by adding fuel
+For each mass in masses, need to repeat the loop until the new fuel needed is <=0
+Initially set fuel = mass as a dummy start
+Then so long as fuel > 0, the loop continues
+A check is made before adding fuel to the total in case it is negative on the last pass
+"""
+
+FT2 = 0
+for mass in masses:
+    fuel = mass
+    while fuel > 0:
+        fuel = (int(fuel/3)) - 2
+        if fuel > 0:
+            FT2 += fuel
+
+print("The answer to part 2 is " + str(FT2))
