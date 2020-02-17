3da7be354df7d26830944a08b4ea2e51baccd9ca
S-Spencer <45274560+S-Spencer@users.noreply.github.com>
2020-02-17 21:59:50 +0000

    Update day1.py
    Code correctly answers the first part of the puzzle

--- day1.py
+++ day1.py
@@ -4,3 +4,21 @@ Open input file and store in list 'masses'
 masses = []
 raw = open("input_1.txt","r")
 for line in raw:
+    masses.append(int(line))
+raw.close()
+
+"""
+Next step calculate the Fuel Total required as the puzzle answer for part one 'FT'
+To calculate fuel for each mass in masses:
+Divide mass by 3
+Take int of the result
+Subtract 2
+Add result to FT
+"""
+
+FT = 0
+for mass in masses:
+    fuel = int(mass/3)
+    FT += (fuel - 2)
+
+print("The answer to part 1 is" + str(FT))
